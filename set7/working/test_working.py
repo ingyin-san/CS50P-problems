68fea39948e136f1b12c6ca1c25ef45dd16ceb7b
ingyin-san <ingyinsan7700@gmail.com>
2024-09-09 19:44:11 +0630

    Create test_working.py

---
+++ set7/working/test_working.py
@@ -0,0 +1,26 @@
+#functions that collectively test your implementation of convert thoroughly
+
+import pytest
+from working import convert
+
+def test_separator():
+    with pytest.raises(ValueError):
+        convert("2 AM - 6 PM")
+    with pytest.raises(ValueError):
+        convert("8:00 AM - 12:00 PM")
+
+def test_hour():
+    with pytest.raises(ValueError):
+        convert("0:30 AM to 2:00 AM")
+    with pytest.raises(ValueError):
+        convert("12:00 PM to 13:00 PM")
+
+    assert convert("9:00 AM to 3:00 PM") == "09:00 to 15:00"
+
+def test_minute():
+    with pytest.raises(ValueError):
+        convert("10:60 AM to 12:00 AM")
+    with pytest.raises(ValueError):
+        convert("3:00 PM to 4:99 PM")
+
+    assert convert("6:30 PM to 8:59 PM") == "18:30 to 20:59"
